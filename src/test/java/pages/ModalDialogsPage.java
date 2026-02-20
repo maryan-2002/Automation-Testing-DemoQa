fb613880c8e936cfc13977f69c73d6c9f29eba18
kareenkhoury <142909297+kareenkhoury@users.noreply.github.com>
2026-02-20 17:08:56 +0200

    Update ModalDialogsPage.java

--- src/test/java/pages/ModalDialogsPage.java
+++ src/test/java/pages/ModalDialogsPage.java
@@ -1,6 +1,5 @@
 package pages;
  
- 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -46,5 +45,4 @@ public class ModalDialogsPage extends BasePage {
  
         jsClick(element);
     }
-
 }
